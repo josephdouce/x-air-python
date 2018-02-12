e7fb2ad35368a8fa5a78f0c34444c3cf1fe018f3
joseph douce <josephdouce@gmail.com>
2018-02-12 23:16:47 +0000

    UI

--- x-air-python.py
+++ x-air-python.py
@@ -1,128 +1,80 @@
-# Import
+# import
 import socket
 import sys
 import time
 import random
 from functools import partial
 
-#Kivy
+# kivy
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label 
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty, StringProperty, ListProperty, NumericProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.slider import Slider
+from kivy.properties import *
+from kivy.graphics import Rectangle, Color
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.config import Config
 
-# Import needed modules from osc4py3
+# import needed modules from osc4py3
 from osc4py3.oscbuildparse import *
 
-# Sockets
+# sockets
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Server
+# server
 server_address = ('192.168.1.101', 10024)
+ 
+# ui size
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '768')
+Config.write()
 
-# Build Message
-msg2 = OSCMessage("/info","",[])
-
-def remap( x, oMin, oMax, nMin, nMax ):
-
-    #range check
-    if oMin == oMax:
-        print("Warning: Zero input range")
-        return None
-
-    if nMin == nMax:
-        print("Warning: Zero output range")
-        return None
-
-    #check reversed input range
-    reverseInput = False
-    oldMin = min( oMin, oMax )
-    oldMax = max( oMin, oMax )
-    if not oldMin == oMin:
-        reverseInput = True
-
-    #check reversed output range
-    reverseOutput = False   
-    newMin = min( nMin, nMax )
-    newMax = max( nMin, nMax )
-    if not newMin == nMin :
-        reverseOutput = True
-
-    portion = (x-oldMin)*(newMax-newMin)/(oldMax-oldMin)
-    if reverseInput:
-        portion = (oldMax-x)*(newMax-newMin)/(oldMax-oldMin)
-
-    result = portion + newMin
-    if reverseOutput:
-        result = newMax - portion
-
-    return result
-
-class XAirPython(FloatLayout):
-    meters = ListProperty([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-
+# builder class
+class XAirPython(BoxLayout):
+    # start all meter values at 0
+    meters = ListProperty([0] * 100)
+    
+    # decode meters 
     def decode_meters(self, data):
-        def meters_0(data):
-            meters = [int.from_bytes(data[-16:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_1(data):
-            meters = [int.from_bytes(data[-80:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_2(data):
-            meters = [int.from_bytes(data[-72:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_3(data):
-            meters = [int.from_bytes(data[-112:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_4(data):
-            meters = [int.from_bytes(data[-200:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_5(data):
-            meters = [int.from_bytes(data[-88:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_6(data):
-            meters = [int.from_bytes(data[-78:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_7(data):
-            meters = [int.from_bytes(data[-32:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_8(data):
-            meters = [int.from_bytes(data[-8:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        def meters_9(data):
-            meters = [int.from_bytes(data[-8:][i:i+2], byteorder='little', signed = True) for i in range(0, 100, 2)]
-            return meters
-        
-        options = {
-           b'0' : meters_0,
-           b'1' : meters_1,
-           b'2' : meters_2,
-           b'3' : meters_3,
-           b'4' : meters_4,
-           b'5' : meters_5,
-           b'6' : meters_6,
-           b'7' : meters_7,
-           b'8' : meters_8,
-           b'9' : meters_9,
+
+        # byte positions for meters
+        meter_positions = {
+           b'meters/0' : -16,
+           b'meters/1' : -80,
+           b'meters/2' : -72,
+           b'meters/3' : -112,
+           b'meters/4' : -200,
+           b'meters/5' : -88,
+           b'meters/6' : -78,
+           b'meters/7' : -32,
+           b'meters/8' : -8,
+           b'meters/9' : -8,
         }
 
-        num = data[7:8]
-        meters = options[num](data)
+        # get byte position
+        num = meter_positions[data[0:8]]
+
+        # convert meters to 0-1 range 
+        meters = [(int.from_bytes(data[num:][i:i+2], byteorder='little', signed = True) + 32768) * 1/65536 for i in range(0, 100, 2)]
+
         return meters
 
+    # subscribe to meters
     def subscribe(self, num, *args):
         msg = OSCMessage("/batchsubscribe",",ssiii",["meters/" + num, "/meters/" + num, 0,0,1])
-        # Send
+        # send
         sent = sock.sendto(encode_packet(msg), server_address)
     
+    # recieve data
     def recieve(self, *args):
-        # Send
+        # connect
         sent = sock.connect(server_address)
 
-        # Recieve
+        # recieve
         data, server = sock.recvfrom(512)
         try:
             print(decode_packet(data))
@@ -130,13 +82,23 @@ class XAirPython(FloatLayout):
             if (data[0:7] == b'meters/'):
                 self.meters = self.decode_meters(data)
 
+# dummy class see kv file
+class Placeholder(BoxLayout):
+  pass
+
+# main class
 class Main(App):
     def build(self):
+        # new instance or XAirPython
         xair = XAirPython()
+        # subscribe to meters
         xair.subscribe('2')
-        Clock.schedule_interval(partial(xair.subscribe, '2'), 10)
-        Clock.schedule_interval(xair.recieve, 1/60)
+        # schedule refresh meter subscription
+        Clock.schedule_interval(partial(xair.subscribe, '2'), 9)
+        # schedule check for incoming data
+        Clock.schedule_interval(xair.recieve, 1/100)
         return xair
 
+# run app
 if __name__ == "__main__":
     Main().run()
